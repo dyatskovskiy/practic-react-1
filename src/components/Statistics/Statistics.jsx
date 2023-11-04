3111125a428c0ac7dcc09efe56c0542c7fbaf648
OlenaKovalenko <elenkow17@gmail.com>
2023-11-04 12:14:10 +0200

    task2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,10 +4,16 @@ import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
 
-export const Statistics = () => {
+export const Statistics = ({title, stats}) => {
   return (
     <>
-      <div>StatisticsList</div>
-    </>
+      {title && <StatisticTitle>{title}</StatisticTitle>}
+      <StatisticsList>
+        {stats.map(({ id, title, total }) => (
+          <StatisticItem title={title} total={total} key={id} />
+        )
+        )}
+      </StatisticsList>
+      </>
   );
 };
